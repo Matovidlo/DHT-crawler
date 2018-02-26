e89bbbc4324fc7411ce89978fc769068d75cb86a
Matovidlo <xvasko12@stud.fit.vutbr.cz>
2018-02-26 15:35:43 +0100

    Change setup.sh, add packages to dockerfile and try first build of docker run

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,22 @@
 #Docker file for containerization of this application
-FROM fedora25
+# TODO add multiple platforms
+
+# fedora 25,26,27,28
+#
+
+FROM fedora:27
 
 MAINTAINER Martin Vasko <xvasko12@stud.fit.vutbr.cz> <matovidlo2@gmail.com>
 
 LABEL description="Docker image with various OS and environment with python3.6 / python3.5"
 
-RUN dnf install -y python3.6 pip
-RUN pip install bencoder hashlib collections socket random
+RUN dnf install -y python36 python3-pip python3-devel gcc redhat-rpm-config
+RUN pip3.6 install bencoder.pyx bencode
+
+COPY . /monitoring/
+# FIXME copy successful
+# RUN ls -la /monitoring/*
+# TODO hashlib
 
 WORKDIR /monitoring
 
--- setup.sh
+++ setup.sh
@@ -13,71 +13,141 @@ help()
 	echo "Help message"
 }
 
+verify()
+{
+	sudo systemctl start docker
+	sudo docker run hello-world
+}
+
+get_os()
+{
+	if [ -f /etc/os-release ]; then
+		# freedesktop.org and systemd
+		. /etc/os-release
+		local OS=$NAME
+		local VER=$VERSION_ID
+	elif type lsb_release >/dev/null 2>&1; then
+		# linuxbase.org
+		local OS=$(lsb_release -si)
+		local VER=$(lsb_release -sr)
+	elif [ -f /etc/lsb-release ]; then
+		# For some versions of Debian/Ubuntu without lsb_release command
+		. /etc/lsb-release
+		local OS=$DISTRIB_ID
+		local VER=$DISTRIB_RELEASE
+	elif [ -f /etc/debian_version ]; then
+		# Older Debian/Ubuntu/etc.
+		local OS=Debian
+		local VER=$(cat /etc/debian_version)
+	elif [ -f /etc/SuSe-release ]; then
+		# Older SuSE/etc.
+		...
+	elif [ -f /etc/redhat-release ]; then
+		# Older Red Hat, CentOS, etc.
+		...
+	else
+		# Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+		local OS=$(uname -s)
+		local VER=$(uname -r)
+	fi
+	echo "$OS"
+}
+
 while [ $# -gt 0 ]
 do
-  opt=$1
-  case $opt in
-    "--help")
-    	help
-    	exit 0
-    	;;
-    "-h")
-      help
-      exit 0
-      ;;
-    "install")
-      # NEED root pass
-      if [ -f /etc/os-release ]; then
-        # freedesktop.org and systemd
-        . /etc/os-release
-        OS=$NAME
-        VER=$VERSION_ID
-      elif type lsb_release >/dev/null 2>&1; then
-        # linuxbase.org
-        OS=$(lsb_release -si)
-        VER=$(lsb_release -sr)
-      elif [ -f /etc/lsb-release ]; then
-        # For some versions of Debian/Ubuntu without lsb_release command
-        . /etc/lsb-release
-        OS=$DISTRIB_ID
-        VER=$DISTRIB_RELEASE
-      elif [ -f /etc/debian_version ]; then
-        # Older Debian/Ubuntu/etc.
-        OS=Debian
-        VER=$(cat /etc/debian_version)
-      elif [ -f /etc/SuSe-release ]; then
-        # Older SuSE/etc.
-        ...
-      elif [ -f /etc/redhat-release ]; then
-        # Older Red Hat, CentOS, etc.
-        ...
-      else
-        # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
-        OS=$(uname -s)
-        VER=$(uname -r)
-      fi
-      # FIXME we got all necessary
-      echo -e "Operation system: " . $OS . " Version: " . $VER . "\n"
-      echo "Remove older version of docker"
-      echo -e "For next steps it is necessary to give sudo password!\n(Removal of old docker vesion, installing new one)"
-      if [ "$OS" == "Fedora" ]; then
-        sudo dnf remove docker docker-common docker-selinux docker-engine-selinux docker-engine
-        sudo dnf -y install dnf-plugins-core
-        sudo dnf confg-manager \
-          --add-repo \
-          https://download.docker.com/linux/fedora/docker-ce.repo
-        sudo dnf -y install docker-devel
-      fi
-      exit 0
-      ;;
-    "run")
-      # RUN program
-      echo "Run "
-      exit 0
-      ;;
-    "test")
-      echo "Start unitest"
-      exit 0
-  esac
-  shift
+	opt=$1
+	case $opt in
+		"--help")
+			help
+			exit 0
+			;;
+		"-h")
+			help
+			exit 0
+			;;
+		"install-docker")
+			# NEED root pass
+			OS=$(get_os)
+			# FIXME we got all necessary
+			echo -e "Operation system: " . $OS . " Version: " . $VER . "\n"
+			echo "Remove older version of docker"
+			echo -e "For next steps it is necessary to give sudo password!\n(Removal of old docker vesion, installing new one)"
+			if [ "$OS" == "Fedora" ]; then
+				verify=false
+				case "$2" in
+					"--remove_old")
+						sudo dnf remove docker docker-common docker-selinux docker-engine-selinux docker-engine
+						sudo dnf -y install dnf-plugins-core
+						;;
+					# verify that all is installed correctly
+					"--verify")
+						verify=true
+						;;
+				esac
+				# alias
+				case "$3" in
+					"--remove_old")
+						sudo dnf remove docker docker-common docker-selinux docker-engine-selinux docker-engine
+						sudo dnf -y install dnf-plugins-core
+						;;
+					# verify that all is installed correctly
+					"--verify")
+						verify=true
+						;;
+				esac
+				# Install
+				sudo dnf config-manager \
+					--add-repo \
+					https://download.docker.com/linux/fedora/docker-ce.repo
+				sudo dnf -y install docker-devel docker-ce
+				# Verify
+				# user=$(whoami)
+				# echo $user
+				# $USER
+				sudo groupadd docker
+				sudo usermod -aG docker $USER
+				session=$(echo $DESKTOP_SESSION | grep -Eo gnome)
+				if [ "$session" == "gnome" ]; then
+					gnome-session-quit
+				fi
+
+				if [ $verify = true ]; then
+					verify
+				fi
+
+			fi
+			exit 0
+			;;
+
+		"install")
+			OS=$(get_os)
+			echo -e "Operation system: " . $OS
+			if [ "$OS" == "Fedora" ]; then
+				sudo dnf -y install python-devel python36
+				pip3.6 install bencoder hashlib
+			fi
+			exit 0
+			;;
+		"run")
+			# RUN program
+			./src/monitor.py
+			# TODO run with various arguments
+			exit 0
+			;;
+		"run-docker")
+			sudo systemctl status docker &> /dev/null
+			if [ $? == 3 ]; then
+				sudo systemctl start docker
+			fi
+			docker build -t monitor-dht .
+			docker run --rm --name monitor-dht -v /$(pwd):/monitoring monitor-dht ./setup.sh run
+			exit 0
+			;;
+		"test")
+			echo "Start unitest"
+			exit 0
+	esac
+	shift
 done
+
+exit 0
